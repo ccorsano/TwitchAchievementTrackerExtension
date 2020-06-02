0511b46c206da5e1be4a05d4ce4b12f55cd000f7
Christian Corsano <christian.corsano@gmail.com>
2020-06-02 22:10:47 +0200

    Add extension frontend project

---
+++ TwitchAchivementTrackerFrontend/public/viewer.js
@@ -0,0 +1,127 @@
+var token = "";
+var tuid = "";
+var ebs = "";
+var detailsVisible = false;
+
+// because who wants to type this every time?
+var twitch = window.Twitch.ext;
+
+// create the request options for our Twitch API calls
+var requests = {
+    titleInfo: createAchievementsRequest('GET', 'title', updateTitle),
+    summary: createAchievementsRequest('GET', 'summary', updateSummary),
+    listAchievement: createAchievementsRequest('GET', 'list', updateBlock)
+};
+
+function createAchievementsRequest(type, method, callback) {
+    twitch.rig.log('Create request ' + location.protocol + '//localhost:8081/api/achievements/' + method);
+    return {
+        type: type,
+        url: location.protocol + '//localhost:8081/api/achievements/' + method,
+        success: callback,
+        error: logError
+    }
+}
+
+function setAuth(token) {
+    Object.keys(requests).forEach((req) => {
+        twitch.rig.log('Setting auth headers');
+        requests[req].headers = { 'Authorization': 'Bearer ' + token }
+    });
+}
+
+twitch.onContext(function(context) {
+    twitch.rig.log(context);
+});
+
+twitch.onAuthorized(function(auth) {
+    // save our credentials
+    token = auth.token;
+    tuid = auth.userId;
+
+    // enable the button
+    $('#cycle').removeAttr('disabled');
+
+    setAuth(token);
+    $.ajax(requests.titleInfo);
+    $.ajax(requests.summary);
+    // $.ajax(requests.listAchievement);
+
+    $('#showDetails').click(function() {
+        if (detailsVisible)
+        {
+            $("#list").css('display', 'none')
+            detailsVisible = false;
+        }
+        else
+        {
+            $("#list").css('display', 'block')
+            $.ajax(requests.listAchievement)
+            detailsVisible = true;
+        }
+    });
+});
+
+function updateTitle(titleInfo) {
+    $('#gameTitle').text(titleInfo.productTitle);
+    $('#gameLogo').attr('src', titleInfo.logoUri);
+}
+
+function updateSummary(summary) {
+    twitch.rig.log('Updating summary');
+    twitch.rig.log('Summary : ' + summary.completed + '/' + summary.total + ', ' + summary.currentPoints + '/' + summary.totalPoints + ' gamerscore');
+    var percentage = (summary.completed / summary.total) * 100.0;
+    $('#completionHeadline').text('Achievements: ' + summary.completed + '/' + summary.total + ' ' + percentage.toFixed(2) + '%');
+}
+
+function updateBlock(achievements) {
+    var container = $('#list');
+    achievements.forEach(achievement => {
+        var listItem = document.createElement("li");
+        var nameDiv = document.createElement("div");
+        nameDiv.className = "achievementTitle";
+        var descriptionDiv = document.createElement("div");
+        descriptionDiv.className = "achievementDescription";
+        var progressIcon = "";
+        switch (achievement.progressState) {
+            case "Achieved":
+                listItem.className = "completed";
+                $(descriptionDiv).text(achievement.description);
+                break;
+            default:
+                listItem.className = "notCompleted";
+                $(descriptionDiv).text(achievement.lockedDescription);
+                break;
+        }
+        $(nameDiv).text(achievement.name);
+        $(listItem).append(nameDiv);
+        $(listItem).append(descriptionDiv);
+        container.append(listItem);
+    });
+}
+
+function logError(_, error, status) {
+  twitch.rig.log('EBS request returned '+status+' ('+error+')');
+}
+
+function logSuccess(hex, status) {
+  // we could also use the output to update the block synchronously here,
+  // but we want all views to get the same broadcast response at the same time.
+  twitch.rig.log('EBS request returned '+hex+' ('+status+')');
+}
+
+$(function() {
+
+    // when we click the cycle button
+    $('#cycle').click(function() {
+        if(!token) { return twitch.rig.log('Not authorized'); }
+        twitch.rig.log('Requesting a color cycle');
+        $.ajax(requests.set);
+    });
+
+    // listen for incoming broadcast message from our EBS
+    twitch.listen('broadcast', function (target, contentType, color) {
+        twitch.rig.log('Received broadcast color');
+        updateBlock(color);
+    });
+});
