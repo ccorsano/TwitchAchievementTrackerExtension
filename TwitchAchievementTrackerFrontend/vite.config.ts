df0b9794ef198018f434b06045ab2b6dcfed7ace
Christian Corsano <christian.corsano@gmail.com>
2022-08-31 21:09:22 +0200

    Vite: enable sourcemaps

--- TwitchAchievementTrackerFrontend/vite.config.ts
+++ TwitchAchievementTrackerFrontend/vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
                 mobile: resolve(__dirname, 'mobile.html')
             }
         },
+        sourcemap: true,
         assetsDir: "."
     },
     server: {
