df2adfb613a71e0dbd850d0a8854d0eb59c856ca
Christian Corsano <christian.corsano@gmail.com>
2024-10-23 00:16:36 +0200

    Fix XApi error message deserialization

--- TwitchAchievementTrackerBackend/TwitchAchievementTrackerBackend/Model/XApi/XApiError.cs
+++ TwitchAchievementTrackerBackend/TwitchAchievementTrackerBackend/Model/XApi/XApiError.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace TwitchAchievementTrackerBackend.Model.XApi
@@ -9,6 +10,7 @@ namespace TwitchAchievementTrackerBackend.Model.XApi
     {
         public bool Success { get; set; }
         public int Error_Code { get; set; }
+        [JsonPropertyName("message")]
         public string Error_Message { get; set; }
     }
 }
